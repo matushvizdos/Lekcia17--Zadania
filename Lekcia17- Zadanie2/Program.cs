647f26cc1ce1000ba8b2751d93f82c5244ac2c18
Matúš Hvizdoš <matus.hvizdos@gmail.com>
2022-12-13 22:26:52 +0100

    Add project files.

---
+++ Lekcia17- Zadanie2/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> mena = new List<string>();
+mena.Add("Lionel Messi");
+mena.Add("Cristiano Ronaldo");
+mena.Add("Karim Benzema");
+mena.Add("Neymar");
+mena.Add("Erling Braut Haaland");
+mena.Add("Kylian Mbappé");
+mena.Add("Theo Walcott");
+Console.WriteLine("Zoznam futbalistov:\n");
+foreach (string meno in mena)
+    Console.WriteLine(meno);
+
+mena.Sort();
+Console.WriteLine("\nZoznam futbalistov, zoradenych podla mena:\n");
+
+foreach (string meno in mena)
+    Console.WriteLine(meno);
